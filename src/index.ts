7c6528bc0a120f2bd9d9511d40cf49bd1bc77e5f
AndresGonzalez10 <andres.edu.glez@gmail.com>
2026-03-09 00:00:16 -0600

    Fix: Ejecutar initAdmin al arrancar el servidor

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+initAdmin();
 
 app.listen(PORT as number, '0.0.0.0', () => {
   console.log(`Servidor de Usuarios corriendo en el puerto ${PORT}`);
